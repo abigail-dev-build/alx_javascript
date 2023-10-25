5b6039345eb6c46837ed0b17285f5ea057d67ef4
Abigail Folarin <folarinabigail2020@gmail.com>
2023-10-25 22:13:38 +0100

    task-5

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,34 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        this.width = 0;
-        this.height = 0;
-      } else {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print() {
-      if (this.width === 0 || this.height === 0) {
-        console.log("Empty Rectangle");
-      } else {
-        for (let i = 0; i < this.height; i++) {
-          console.log("X".repeat(this.width));
-        }
-      }
-    }
-  
-    rotate() {
-      [this.width, this.height] = [this.height, this.width];
-    }
-  
-    double() {
-      this.width *= 2;
-      this.height *= 2;
-    }
-  }
+
+const Rectangle = require ('./4-rectangle');
   
   class Square extends Rectangle {
     constructor(size) {
@@ -37,27 +9,5 @@ class Rectangle {
     }
   }
   
-  // Example of creating an instance of the Rectangle class
-//   const myRectangle = new Rectangle(5, 3);
-  
-  // Printing the original rectangle
-//   console.log("Original Rectangle:");
-//   myRectangle.print();
-  
-  // Rotating the rectangle
-//   myRectangle.rotate();
-//   console.log("\nAfter Rotating:");
-//   myRectangle.print();
-  
-  // Doubling the dimensions of the rectangle
-//   myRectangle.double();
-//   console.log("\nAfter Doubling:");
-//   myRectangle.print();
-  
-  // Example of creating an instance of the Square class
-//   const mySquare = new Square(4);
-  
-  // Printing the square
-//   console.log("\nSquare:");
-//   mySquare.print();
+module.exports = Square;
   
