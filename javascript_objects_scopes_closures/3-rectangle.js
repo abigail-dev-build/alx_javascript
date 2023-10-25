b5f827b2350588313de6019c18276753a877b4d1
Abigail Folarin <folarinabigail2020@gmail.com>
2023-10-25 22:06:22 +0100

    task-3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,35 +1,20 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        // If w or h is 0 or not a positive integer, create an empty object
-        this.width = 0;
-        this.height = 0;
-      } else {
-        this.width = w;
-        this.height = h;
-      }
+    constructor (w, h) {
+        if (w && h > 0) {
+            this.width = w;
+            this.height = h;
+        }else {
+            return 'Rectangle {}';
+        }
     }
   
     print() {
-      if (this.width === 0 || this.height === 0) {
-        console.log("Empty Rectangle");
-      } else {
-        for (let i = 0; i < this.height; i++) {
+        for (let i = 0; i <= this.height; i++) {
           console.log("X".repeat(this.width));
         }
-      }
     }
   }
   
-  // Example of creating instances of the Rectangle class
-//   const validRectangle = new Rectangle(5, 3);
-//   const invalidRectangle = new Rectangle(0, 8);
-  
-  // Printing rectangles
-//   console.log("Valid Rectangle:");
-//   validRectangle.print();
-  
-//   console.log("\nInvalid Rectangle:");
-//   invalidRectangle.print();
+  module.exports = Rectangle;
   
