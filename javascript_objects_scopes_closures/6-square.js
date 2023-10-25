da282c99f489fe655d13f905c1b50cade39b9cd2
Abigail Folarin <folarinabigail2020@gmail.com>
2023-10-25 22:17:39 +0100

    task-6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,36 +1,8 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        this.width = 0;
-        this.height = 0;
-      } else {
-        this.width = w;
-        this.height = h;
-      }
-    }
+
+const SSquare = require ('./5-square');
   
-    print() {
-      if (this.width === 0 || this.height === 0) {
-        console.log("Empty Rectangle");
-      } else {
-        for (let i = 0; i < this.height; i++) {
-          console.log("X".repeat(this.width));
-        }
-      }
-    }
-  
-    rotate() {
-      [this.width, this.height] = [this.height, this.width];
-    }
-  
-    double() {
-      this.width *= 2;
-      this.height *= 2;
-    }
-  }
-  
-  class Square extends Rectangle {
+  class Square extends SSquare {
     constructor(size) {
       // Call the constructor of the parent class (Rectangle) using super()
       super(size, size);
@@ -50,14 +22,5 @@ class Rectangle {
     }
   }
   
-  // Example of creating an instance of the Square class
-//   const mySquare = new Square(4);
-  
-  // Printing the square using default character X
-//   console.log("Square:");
-//   mySquare.charPrint();
-  
-  // Printing the square using custom character #
-//   console.log("\nSquare with Custom Character:");
-//   mySquare.charPrint('#');
+  module.exports = Square;
   
