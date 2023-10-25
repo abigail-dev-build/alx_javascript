8ce85a67e0b6bb0a5810c866f4405c3cd1fe46b3
Abigail Folarin <folarinabigail2020@gmail.com>
2023-10-25 22:02:08 +0100

    task-2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        // If w or h is 0 or not a positive integer, create an empty object
-        this.width = 0;
-        this.height = 0;
-      } else {
-        this.width = w;
-        this.height = h;
-      }
+    constructor (w, h) {
+        if (w && h > 0) {
+            this.width = w;
+            this.height = h;
+        }else {
+            return 'Rectangle {}';
+        }
     }
   }
   
