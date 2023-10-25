6405fea82e0fdc995e9ced191a644ce1cd90d05c
Abigail Folarin <folarinabigail2020@gmail.com>
2023-10-25 22:11:08 +0100

    task-4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,24 +1,18 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        // If w or h is 0 or not a positive integer, create an empty object
-        this.width = 0;
-        this.height = 0;
-      } else {
-        this.width = w;
-        this.height = h;
-      }
+    constructor (w, h) {
+        if (w && h > 0) {
+            this.width = w;
+            this.height = h;
+        }else {
+            return 'Rectangle {}';
+        }
     }
   
     print() {
-      if (this.width === 0 || this.height === 0) {
-        console.log("Empty Rectangle");
-      } else {
         for (let i = 0; i < this.height; i++) {
           console.log("X".repeat(this.width));
         }
-      }
     }
   
     rotate() {
@@ -33,20 +27,5 @@ class Rectangle {
     }
   }
   
-  // Example of creating an instance of the Rectangle class
-//   const myRectangle = new Rectangle(5, 3);
-  
-  // Printing the original rectangle
-//   console.log("Original Rectangle:");
-//   myRectangle.print();
-  
-  // Rotating the rectangle
-//   myRectangle.rotate();
-//   console.log("\nAfter Rotating:");
-//   myRectangle.print();
-  
-  // Doubling the dimensions of the rectangle
-//   myRectangle.double();
-//   console.log("\nAfter Doubling:");
-//   myRectangle.print();
+module.exports = Rectangle;
   
